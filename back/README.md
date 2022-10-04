0049c449768cc5d98355a32153eb4855bb5301c3
morgane <morganeancelin@me.com>
2022-10-04 15:03:12 +0200

    readmeback

---
+++ back/README.md
@@ -0,0 +1,2 @@
+# npm start #
+From the "back" folder of the project, run `npm start`
--- back/server.js
+++ back/server.js
@@ -1,15 +1,16 @@
 const express = require('express');
-const userRoutes = require('./routes/user.routes');
-const sauceRoutes= require('./routes/sauce.routes');
-require('dotenv').config({ path: './config/.env' });
-require('./config/db');
-const cors = require('cors');
-const path = require('path');
 const app = express();
 app.use(express.json());
 
+require('dotenv').config({ path: './config/.env' });
+require('./config/db');
+
+const cors = require('cors');
+const path = require('path');
 app.use(cors());
 
+const userRoutes = require('./routes/user.routes');
+const sauceRoutes= require('./routes/sauce.routes');
 
 app.use('/api/auth', userRoutes);
 app.use('/api/sauces', sauceRoutes);
